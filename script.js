6bb0a4e174d8a198638344324563cb80696eb00e
Naveen George Joseph <naveengjoseph@gmail.com>
2023-04-28 01:02:55 +0530

    active tab changes fixed

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-
 $(document).on("click", 'a[href^="#"]', function (event) {
   event.preventDefault();
 
@@ -19,3 +18,41 @@ $(document).on("click", 'a[href^="#"]', function (event) {
     500
   );
 });
+
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  if (scroll >= aboutACTIVE) {
+    document.getElementById("aboutTab").classList.add("ACTIVE");
+    document.getElementById("galTab").classList.remove("ACTIVE");
+    document.getElementById("homeTab").classList.remove("ACTIVE");
+  }
+  if (scroll >= galACTIVE && scroll < aboutACTIVE) {
+    document.getElementById("galTab").classList.add("ACTIVE");
+    document.getElementById("aboutTab").classList.remove("ACTIVE");
+    document.getElementById("homeTab").classList.remove("ACTIVE");
+  }
+  if (scroll >= 0 && scroll <= galACTIVE) {
+    document.getElementById("galTab").classList.remove("ACTIVE");
+    document.getElementById("aboutTab").classList.remove("ACTIVE");
+    document.getElementById("homeTab").classList.add("ACTIVE");
+  }
+  
+});
+
+let height = screen.height;
+
+
+var galleryHeight = document
+  .getElementById("galleryLink")
+  .getBoundingClientRect().top;
+let galACTIVE = galleryHeight + window.scrollY;
+
+
+var aboutHeight = document
+  .getElementById("aboutLink")
+  .getBoundingClientRect().top;
+let aboutACTIVE = aboutHeight + window.scrollY;
+
+
+console.log(this.scrollY);
+console.log(aboutACTIVE);
